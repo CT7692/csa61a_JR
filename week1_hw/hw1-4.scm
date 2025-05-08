8a49f9873d06583dec4b3a67b9b4c819cbbe34a5
Joseph Rydel <79807440+CT7692@users.noreply.github.com>
2025-05-08 14:13:32 -0500

    Create hw1-4.scm

---
+++ week1_hw/hw1-4.scm
@@ -0,0 +1,12 @@
+;This is a predicate that iterates through a list of numbers
+;that returns true if they're in ascending order and false otherwise.
+
+(define (orderIter numList)
+  (cond ((< (first numList) (last numList)) #t)
+        ((< (first numList) (first (bf numList)))
+         (orderIter (bf numList)))
+        ((> (first numList) (first (bf numList)))
+         #f)))
+
+(define (ordered? myList)
+  (orderIter myList))
